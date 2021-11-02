3f57da87a073544fed5f86efd15c2a012eb57e26
dat-adi <dat.adithya@gmail.com>
2021-11-03 01:22:18 +0530

    Updated Cargo.toml and main
    Modular dependencies have been updated.
    Main has been modified to fix a few bugs.

--- github-api/src/main.rs
+++ github-api/src/main.rs
@@ -1,29 +1,56 @@
 // Importing the standard library
 // for env which works with the process
-use exitfailure::ExitFailure;
+use serde::{Deserialize, Serialize};
 use std::env;
-use reqwest::Url;
 
-struct User{
-    username: String,
-    repos: i64
+// Defining a structure with JSON compatibility
+#[derive(Serialize, Deserialize, Debug)]
+pub struct User{
+    pub name: String,
+    pub public_repos: i64
 }
 
+// Defining a endpoint location as a constant
+const GITHUB_ENDPOINT: &str = "https://api.github.com/users/";
+
+// Addition of the get function 
+// as part of the User Implementation
 impl User{
-    async fn get(username: &String) -> Result<Self, ExitFailure>{
+    /*
+     *
+     * Arguments is the username in the form of a string
+     * Returns the User object and a standard error?
+     * Unsure why Box and dyn is used.
+     *
+     */
+    async fn get(username: &String) -> Result<User, Box<dyn std::error::Error>>{
+        // Formatting the string to create a path
         let url = format!(
-            "https://api.github.com/users/{}",
-            username
+            "{}{}", GITHUB_ENDPOINT, username
         );
 
-        let url = Url::parse(&url);
-        let res = reqwest::get(url).await?.json::<User>().await?;
 
-        Ok(res);
+        // Parses the url and provides it's value to the variable
+        let client = reqwest::Client::new();
+        let res = client
+            .get(url)
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        // TODO there's a fix to be made in here.
+        // Error("expected value")
+        let user: User = serde_json::from_str(&res).unwrap();
+
+        Ok(user)
     }
 }
 
-fn main(){
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Defining a vector that can take arguments
     // and a collect that converts the iterable into the vector
     let args:Vec<String> = env::args().collect();
@@ -40,6 +67,11 @@ fn main(){
         username = args[1].clone();
     }
 
+    // Calling the async function
+    let user = User::get(&username).await?;
+
     // Printing the arguments
-    println!("{:?}", username);
+    println!("User : {}\tRepositories : {}", user.name, user.public_repos);
+
+    Ok(())
 }
